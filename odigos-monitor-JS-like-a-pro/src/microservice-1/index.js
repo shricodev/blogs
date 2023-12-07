d21fbfac72ea7b4f061cace989c5a7355612b160
Shrijal Acharya <shrijal.acharya@gmail.com>
2023-12-07 15:11:27 +0545

    Create the two microservices for demonstration

---
+++ odigos-monitor-JS-like-a-pro/src/microservice-1/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+
+app.get("/api/data", async (req, res) => {
+  try {
+    const response = await fetch("http://microservice2:3002/api/data");
+    const data = await response.json();
+    res.json({
+      data: "Microservice 2 data received in Microservice 1",
+      microservice2Data: data,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Microservice 1 listening on port ${PORT}`);
+});
